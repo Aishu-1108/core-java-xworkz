6b9cc3b732d6dc79f278aff69a5c977f90681970
Aishu-1108 <aishwaryaanand1108@gmail.com>
2023-07-13 14:07:33 +0530

    committing arrays and for loop files

---
+++ SportsComplex.java
@@ -0,0 +1,22 @@
+class SportsComplex{
+	
+	public static void main(String sport[]){
+		
+		String sportsItems[] = {"Swimming dress","Cricket bat","Tennis Ball","Foot Ball","Basket Ball"};
+		
+		System.out.println("Main started");
+		
+		for(String sportsItem:sportsItems){
+			
+			System.out.println(sportsItem);
+		}
+		
+		System.out.println("Main started");
+		
+		
+		
+	}
+	
+	
+	
+}
---
+++ SuperMarket.java
@@ -0,0 +1,109 @@
+class SuperMarket{
+	
+    // show 5 vegetables
+    static String vegetable1 = "Potato";
+	static String vegetable2 = "Carrot";
+	static String vegetable3 = "Beans";
+	static String vegetable4 = "Brinjal";
+	static String vegetable5 = "Onion";
+	
+	//array variable
+	static String vegetable[]={"Potato","Carrot","Beans","Brinjal","Onion"};
+	
+	// biscuits
+	static String biscuit1 = "Unibic";
+	static String biscuit2 = "Marigold";
+	static String biscuit3 = "Monaco";
+	static String biscuit4 = "Parle-G";
+	static String biscuit5 = "Gooday";
+	static String biscuit6 = "MomsMagic";
+	static String biscuit7 = "Coconut Biscuit";
+	static String biscuit8 = "Dark Fantasy";
+	static String biscuit9 = "Jim Jam";
+	static String biscuit10 = "Butter Cookies";
+	
+    // perfumes - 10
+	static String perfume1 = "Spinz";
+	static String perfume2 = "Watergirl";
+	static String perfume3 = "Engage";
+	static String perfume4 = "Fogg";
+	static String perfume5 = "Lotus";
+	static String perfume6 = "Eva";
+	static String perfume7 = "Axe";
+	static String perfume8 = "Tommy Girl";
+	static String perfume9 = "Shine";
+	static String perfume10 = "Mysore Sandal";
+	
+   // groceries - 10
+	static String grocery1 = "Rice";
+	static String grocery2 = "Ragi";
+	static String grocery3 = "Dhal";
+	static String grocery4 = "Moog Dhal";
+	static String grocery5 = "Green Gram";
+	static String grocery6 = "Horse gram";
+	static String grocery7 = "Salt";
+	static String grocery8 = "Sugar";
+	static String grocery9 = "Rava";
+	static String grocery10 = "Coffee Powder";
+	
+	public static void main(String market[]){
+		
+		System.out.println("Main started");
+		System.out.println("-------------------------------");
+		
+		System.out.println("List of vegetables");
+		System.out.println("1st Rack "+ vegetable1);
+		System.out.println("2nd Rack "+ vegetable2);
+		System.out.println("3rd Rack "+ vegetable3);
+		System.out.println("4th Rack "+ vegetable4);
+		System.out.println("5th Rack "+ vegetable5);
+		System.out.println(" ");
+		
+		System.out.println("List of biscuits");
+		System.out.println("1st row "+ biscuit1);
+		System.out.println("2nd row "+ biscuit2);
+		System.out.println("3rd row "+ biscuit3);
+		System.out.println("4th row "+ biscuit4);
+		System.out.println("5th row "+ biscuit5);
+		System.out.println("6th row "+ biscuit6);
+		System.out.println("7th row "+ biscuit7);
+		System.out.println("8th row "+ biscuit8);
+		System.out.println("9th row "+ biscuit9);
+		System.out.println("10th row "+ biscuit10);
+		System.out.println(" ");
+		
+		System.out.println("List of perfumes");
+		System.out.println("1st Rack "+ perfume1);
+		System.out.println("2nd Rack "+ perfume2);
+		System.out.println("3rd Rack "+ perfume3);
+		System.out.println("4th rack "+ perfume4);
+		System.out.println("5th rack "+ perfume5);
+		System.out.println("6th rack "+ perfume6);
+		System.out.println("7th tack "+ perfume7);
+		System.out.println("8th rack "+ perfume8);
+		System.out.println("9th rack "+ perfume9);
+		System.out.println("10th rack "+ perfume10);
+		System.out.println(" ");
+		
+		System.out.println("List of Groceries");
+		System.out.println("1st bag "+ grocery1);
+		System.out.println("2nd bag "+ grocery2);
+		System.out.println("3rd bag "+ grocery3);
+		System.out.println("4th bag "+ grocery4);
+		System.out.println("5th bag "+ grocery5);
+		System.out.println("6th bag "+ grocery6);
+		System.out.println("7th bag "+ grocery7);
+		System.out.println("8th bag "+ grocery8);
+		System.out.println("9th bag "+ grocery9);
+		System.out.println("10th bag "+ grocery10);
+		System.out.println("------------------------------");
+		
+		System.out.println("Main ended");
+	
+	}
+ 
+}
+
+
+
+
