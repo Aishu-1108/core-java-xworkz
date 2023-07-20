ecad775b5a07a6c6bf18264ff4f770bde158adb9
Aishu-1108 <aishwaryaanand1108@gmail.com>
2023-07-20 11:09:37 +0530

    committing technical seminar file

---
+++ Example.java
@@ -0,0 +1,9 @@
+class Main{
+   static int age[]={12,45,32,23};
+   public static void main(String args[]){
+     System.out.println("Using loops");
+     for(int i=0;i<age.length;i++){
+		 System.out.println(age[i]);
+	 }	 
+   }
+}
